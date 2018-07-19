877f2b7ca10dc62828f648bacfb643c2b0899eea
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-07-19 14:53:20 -0600

    add storysource to view source code

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.stories\.jsx?$/,
+        loaders: [require.resolve('@storybook/addon-storysource/loader')],
+        enforce: 'pre',
+      },
+    ],
+  },
+};
