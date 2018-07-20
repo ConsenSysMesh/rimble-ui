52dcf55ac72a71f0ca7fbc37485d0ca2be1fa076
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-07-19 23:03:56 -0600

    remove hello world story

--- src/HelloWorld/index.stories.js
+++ src/HelloWorld/index.stories.js
@@ -4,4 +4,4 @@ import { storiesOf } from '@storybook/react'
 
 import HelloWorld from '../HelloWorld'
 
-storiesOf('HelloWorld', module).add('simple component', () => <HelloWorld />)
+// storiesOf('HelloWorld', module).add('simple component', () => <HelloWorld />)
