acbe39fd340de008b1729555a8073d2e92a3c1e9
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-07-19 23:04:22 -0600

    add slider component

---
+++ src/Slider/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Slider = styled.input.attrs({ type: 'range' })`
+
+`
+
+export default Slider
---
+++ src/Slider/index.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import Slider from '../Slider'
+
+storiesOf('Slider', module).add('default', () => <Slider />)
