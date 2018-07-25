aef949523de5837e73bb54955e19cfb96659edae
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-07-25 14:08:00 -0600

    add checkbox component

---
+++ src/Checkbox/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Checkbox = styled.input.attrs({ type: 'checkbox' })`
+  font-weight: bold;
+`
+
+export default Checkbox
---
+++ src/Checkbox/index.stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import Checkbox from '../Checkbox'
+
+storiesOf('Checkbox', module).add('default', () => (
+  <div>
+    <label htmlFor="">
+      <Checkbox id="blue" name="pill"/>
+      Blue Pill
+    </label>
+    <label htmlFor="">
+      <Checkbox id="red" name="pill" />
+      Red Pill
+    </label>
+  </div>
+))
