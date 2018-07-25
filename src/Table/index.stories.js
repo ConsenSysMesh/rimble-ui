70f5069cfad8ed1376c6a6e0fb17425630f577db
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-07-25 16:45:42 -0600

    add table component , wip

---
+++ src/Table/index.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import Table from '../Table'
+
+storiesOf('Table', module)
+  .add('default', () => (
+    <Table />
+  ))
