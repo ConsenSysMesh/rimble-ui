d51a15158b650606973305ba66b5cf17e23656f1
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-07-25 14:06:14 -0600

    add input component

---
+++ src/Input/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Input = styled.input`
+  font-weight: bold;
+`
+
+export default Input
---
+++ src/Input/index.stories.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import Input from '../Input'
+
+storiesOf('Input', module)
+  .add('text', () => (
+    <Input type='text' />
+  )).add('email', () => (
+    <Input type='email' name='email' id='email' placeholder='you@example.com'/>
+  )).add('number', () => (
+    <Input type='number'/>
+  )).add('password', () => (
+    <Input type='password'/>
+  )).add('search', () => (
+    <Input type='search'/>
+  )).add('tel', () => (
+    <Input type='tel'/>
+  )).add('url', () => (
+    <Input type='url'/>
+  ))
