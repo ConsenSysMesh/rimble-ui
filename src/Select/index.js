9bd97846d97d4a34e525b36ab7c877fd225706d8
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-10-04 15:28:51 -0600

    styled Select

--- src/Select/index.js
+++ src/Select/index.js
@@ -2,8 +2,33 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledSelect = styled.select`
-  display: block;
-  ${'' /* font-family: inherit; */}
+  ${'' /* display: block; */}
+  appearance: none;
+  font-size: 1rem;
+  border-radius: ${ props => props.theme.radii[0] };
+
+  height: 48px;
+  padding: 0 3rem 0 1rem;
+
+  /* Rectangle 3: */
+  background: #FFFFFF;
+  border: 1px solid #CCCCCC;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.10);
+
+  &:hover {
+
+  }
+  &:focus {
+    outline: none;
+    border-color: ${ props => props.theme.colors.primary }
+  }
+  &:active {
+
+  }
+  &:disabled {
+    opacity: 0.4;
+    pointer-events: none;
+  }
 `
 
 const Select = (props) => {
@@ -11,9 +36,7 @@ const Select = (props) => {
     {props.items.map(
       (item, i) => (<option key={i} value={item}>{item}</option>)
     )}
-    {/* {props.children} */}
   </StyledSelect>
 }
-
 
 export default Select
