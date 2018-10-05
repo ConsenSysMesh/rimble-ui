b6284ceefadc374e5b2aaaa1d35a671629084365
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-10-05 17:03:15 -0600

    update text styles

--- src/Text/index.js
+++ src/Text/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  :root {
+    font-family: "Inter UI";
+    font-size: 16px;
+  }
+`
+
+import {
+  fontSize,
+  space,
+  color,
+  responsiveStyle
+} from 'styled-system'
+
+
+
 const Text = styled.div`
-  font-family: sans-serif;
+  ${fontSize} ${space} ${color}
 `
 
 Text.span = Text.withComponent('span')
 Text.p = Text.withComponent('p')
+Text.s = Text.withComponent('s')
 
 export default Text
