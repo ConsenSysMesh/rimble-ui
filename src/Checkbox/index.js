250630e51abed358f873ffd6169f513e1368b60c
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-10-10 14:48:30 -0600

    style checkbox with MD icons

--- src/Checkbox/index.js
+++ src/Checkbox/index.js
@@ -1,8 +1,52 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Checkbox = styled.input.attrs({ type: 'checkbox' })`
-  font-weight: bold;
+import CheckBoxIcon from '../Icon/baseline-check_box-24px'
+import CheckBoxOutlineIcon from '../Icon/baseline-check_box_outline_blank-24px'
+
+const StyledWrapper = styled.div`
+  & {
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  > input {
+    position: relative;
+    display: block;
+    appearance: none;
+    background: white;
+    height: 1.5rem;
+    width: 1.5rem;
+    margin: 0;
+    font-weight: bold;
+  }
+  > input:checked + svg.checked {
+    display: block;
+  }
+
+  > svg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 100%;
+    width: 100%;
+    pointer-events: none;
+  }
+
+  > svg.checked {
+    display: none;
+  }
 `
+
+const Checkbox = ({}) => (
+  <StyledWrapper>
+    <input type="checkbox"/>
+    <CheckBoxIcon className='checked'/>
+    <CheckBoxOutlineIcon className='unchecked'/>
+  </StyledWrapper>
+)
 
 export default Checkbox
