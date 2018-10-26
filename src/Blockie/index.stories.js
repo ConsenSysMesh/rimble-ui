0d74084340dddfd0d7e9939e2ea9e21846b89472
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-10-26 22:02:48 +0200

    replace everything with starter files

--- src/Blockie/index.stories.js
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/react'
-
-import React from 'react'
-import Blockie from '../Blockie'
-
-storiesOf('Blockie', module)
-  .add('Default', () => <Blockie  opts={{seed: "foo", color: "#dfe", bgcolor: "#a71", size: 15, scale: 3, spotcolor: "#000"}} /> )
--- src/Radio/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-
-const Radio = styled.input.attrs({ type: 'radio' })`
-  font-weight: bold;
-`
-
-export default Radio
