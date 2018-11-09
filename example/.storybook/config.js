31668f93e6a85e36f831dadf34271ee323725128
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-11-09 19:28:44 +0100

    remove extra line

--- example/.storybook/config.js
+++ example/.storybook/config.js
@@ -2,7 +2,6 @@ import { configure } from '@storybook/react';
 
 const req = require.context('../src/stories', true, /.js$/);
 
-
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
