2625ae10edba767174c18c781c5348b41c664342
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-11-16 19:04:52 +0000

    configure storybook options

--- example/.storybook/config.js
+++ example/.storybook/config.js
@@ -1,5 +1,6 @@
 import { configure, addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
+import { withOptions } from '@storybook/addon-options';
 
 import React from 'react';
 import { ThemeProvider } from 'rimble-ui'
@@ -7,6 +8,13 @@ import { ThemeProvider } from 'rimble-ui'
 addDecorator(
   withInfo({
     inline: false
+  })
+)
+
+addDecorator(
+  withOptions({
+    name: 'Rimble UI',
+    url: 'https://rimble.consensys.design/',
   })
 )
 
