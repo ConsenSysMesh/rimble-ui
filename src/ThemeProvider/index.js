931f8a80713ed7e95916a0a9cf55fdaa488065f4
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-12-11 17:02:39 -0700

    add selection color to theme provider

--- src/ThemeProvider/index.js
+++ src/ThemeProvider/index.js
@@ -2,23 +2,37 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import styled, {
+  createGlobalStyle,
   ThemeProvider as StyledThemeProvider
 } from 'styled-components'
 
 import theme from '../theme'
 
+import { transparentize } from 'polished'
+
 export const Base = styled.div`
-  font-family: ${props => props.theme.fonts.sansSerif};
-  font-size: 16px;
+  & {
+    font-size: 16px;
+    font-family: ${props => props.theme.fonts.sansSerif};
+  }
   * {
     box-sizing: border-box;
+  }
+`
+
+const GlobalStyle = createGlobalStyle`
+  ::selection {
+    background: ${ props => transparentize(0.7, props.theme.colors.primary) };
   }
 `
 
 const ThemeProvider = ({ ...props }) => {
   return (
     <StyledThemeProvider theme={theme}>
-      <Base {...props} />
+      <React.Fragment>
+        <Base {...props} />
+        <GlobalStyle />
+      </React.Fragment>
     </StyledThemeProvider>
   )
 }
