24a6d66314e7af46bfcd414787088850761a8444
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-12-12 17:54:40 -0700

    add icons to Button component

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import styled from 'styled-components'
 
 import { tint, shade } from 'polished'
+
+import Icon from 'rmdi/lib/Icon'
 
 import {
   space,
@@ -28,43 +29,40 @@ const size = (props) => {
       return `
         font-size: 12px;
         height: ${theme.fontSizes[5]}px;
+        min-width: ${theme.fontSizes[5]}px;
       `
     case 'medium':
       return `
         font-size: 16px;
         height: ${theme.fontSizes[6]}px;
+        min-width: ${theme.fontSizes[6]}px;
       `
     case 'large':
       return `
         font-size: 24px;
         height: ${theme.fontSizes[7]}px;
+        min-width: ${theme.fontSizes[7]}px;
       `
     default:
       return `
         font-size: 16px;
         height: ${theme.fontSizes[6]}px;
+        min-width: ${theme.fontSizes[6]}px;
       `
   }
 }
 
-const Button = styled.button`
-  -webkit-font-smoothing: antialiased;
-  cursor: pointer;
-  position: relative;
-  display: inline-block;
-  vertical-align: middle;
-  text-align: center;
-  text-decoration: none;
-  white-space: nowrap;
-  line-height: 1;
-  font: inherit;
-  color: inherit;
+const MyButton = React.forwardRef((props, ref) => (
+  <button {...props} ref={ref} className={props.className}>
+    { props.icon && !props.iconpos && <Icon name={props.icon} /> }
+    { props.icon && props.iconpos === 'left' && <Icon name={props.icon} /> }
+    { props.children && <span>{props.children}</span> }
+    { props.icon && props.iconpos === 'right' && <Icon name={props.icon} /> }
+  </button>
+))
 
-  transition: all .15s ease;
-  transform-origin: center;
-
+const Button = styled(MyButton)`
   ${size} ${fullWidth}
-
   ${space}
   ${color}
   ${bgColor}
@@ -73,6 +71,28 @@ const Button = styled.button`
   ${borderRadius}
   ${fontWeight}
   ${boxShadow}
+
+  & {
+    -webkit-font-smoothing: antialiased;
+    display: inline-block;
+    position: relative;
+    cursor: pointer;
+    vertical-align: middle;
+    text-align: center;
+    text-decoration: none;
+    white-space: nowrap;
+    line-height: 1;
+    ${'' /* font: inherit; */}
+    ${'' /* color: inherit; */}
+    overflow: hidden;
+
+    transition: all .15s ease;
+
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    padding: ${props => props.icononly ? '0' : props.p };
+  }
 
   &:hover {
     background-color: ${props => tint(0.1, props.theme.colors.primary)};
@@ -87,18 +107,24 @@ const Button = styled.button`
     pointer-events: none;
     cursor: not-allowed;
   }
+
+  svg {
+    display: block;
+  }
+  svg:first-child {
+    margin-left: -1rem;
+    margin-right: .5rem;
+  }
+  svg:last-child {
+    margin-left: .5rem;
+    margin-right: -1rem;
+  }
+  svg:first-child:last-child {
+    margin: 0;
+  }
 `
 
-Button.propTypes = {
-  ...fontWeight.propTypes,
-  ...borders.propTypes,
-  ...borderColor.propTypes,
-  ...borderRadius.propTypes,
-  // ...buttonStyle.propTypes,
-}
-
 Button.defaultProps = {
-  as: 'button',
   theme: theme,
   fontSize: 'inherit',
   fontWeight: 3,
