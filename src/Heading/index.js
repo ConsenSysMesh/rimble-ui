76aac28bcdae1b842d51a14200cff0ad87d62701
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-12-15 16:30:11 -0700

    add font-weights to headings

--- src/Heading/index.js
+++ src/Heading/index.js
@@ -10,6 +10,7 @@ Heading.defaultProps = {
   as: 'h3',
   regular: true,
   fontSize: 4,
+  fontWeight: 3,
   my: 3,
 }
 
@@ -17,6 +18,7 @@ Heading.h1 = styled(Heading)``
 Heading.h1.defaultProps = {
   as: 'h1',
   fontSize: 6,
+  fontWeight: 3,
   my: 3,
 }
 
@@ -24,30 +26,35 @@ Heading.h2 = styled(Heading)``
 Heading.h2.defaultProps = {
   as: 'h2',
   fontSize: 5,
+  fontWeight: 3,
   my: 3,
 }
 Heading.h3 = styled(Heading)``
 Heading.h3.defaultProps = {
   as: 'h3',
   fontSize: 4,
+  fontWeight: 3,
   my: 3,
 }
 Heading.h4 = styled(Heading)``
 Heading.h4.defaultProps = {
   as: 'h4',
   fontSize: 3,
+  fontWeight: 3,
   my: 3,
 }
 Heading.h5 = styled(Heading)``
 Heading.h5.defaultProps = {
   as: 'h5',
   fontSize: 2,
+  fontWeight: 3,
   my: 3,
 }
 Heading.h6 = styled(Heading)``
 Heading.h6.defaultProps = {
   as: 'h6',
   fontSize: 1,
+  fontWeight: 3,
   my: 3,
 }
 
