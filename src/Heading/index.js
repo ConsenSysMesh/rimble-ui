1f22f38403fbf4baccaf57a995ae62a366ecd524
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-12-15 16:19:54 -0700

    update Heading components to use 'as

--- src/Heading/index.js
+++ src/Heading/index.js
@@ -4,42 +4,49 @@ import styled from 'styled-components'
 import Text from '../Text'
 
 // body size + 6 headings
-const Heading = Text.withComponent('h3')
+const Heading = styled(Text)``
 
 Heading.defaultProps = {
+  as: 'h3',
   regular: true,
   fontSize: 4,
   my: 3,
 }
 
-Heading.h1 = Heading.withComponent('h1')
+Heading.h1 = styled(Heading)``
 Heading.h1.defaultProps = {
+  as: 'h1',
   fontSize: 6,
   my: 3,
 }
 
-Heading.h2 = Heading.withComponent('h2')
+Heading.h2 = styled(Heading)``
 Heading.h2.defaultProps = {
+  as: 'h2',
   fontSize: 5,
   my: 3,
 }
-Heading.h3 = Heading.withComponent('h3')
+Heading.h3 = styled(Heading)``
 Heading.h3.defaultProps = {
+  as: 'h3',
   fontSize: 4,
   my: 3,
 }
-Heading.h4 = Heading.withComponent('h4')
+Heading.h4 = styled(Heading)``
 Heading.h4.defaultProps = {
+  as: 'h4',
   fontSize: 3,
   my: 3,
 }
-Heading.h5 = Heading.withComponent('h5')
+Heading.h5 = styled(Heading)``
 Heading.h5.defaultProps = {
+  as: 'h5',
   fontSize: 2,
   my: 3,
 }
-Heading.h6 = Heading.withComponent('h6')
+Heading.h6 = styled(Heading)``
 Heading.h6.defaultProps = {
+  as: 'h6',
   fontSize: 1,
   my: 3,
 }
