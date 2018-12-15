14ddf060745eb5ec9e64417925f985de4b30f3b5
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-12-15 16:46:32 -0700

    add Flex component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as Box } from './Box'
+export { default as Flex } from './Flex'
 export { default as Button } from './Button'
 export { default as Avatar } from './Avatar'
 export { default as Blockie } from './Blockie'
