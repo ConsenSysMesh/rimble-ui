77dbfb8dbf4272522f0065b1a0589bdf76f3ac32
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-12-20 18:54:02 -0700

    add text-align props

--- src/Heading/index.js
+++ src/Heading/index.js
@@ -8,10 +8,10 @@ const Heading = styled(Text)``
 
 Heading.defaultProps = {
   as: 'h3',
-  regular: true,
   fontSize: 4,
   fontWeight: 3,
-  my: 3,
+  textAlign: 'left',
+  my: 3
 }
 
 Heading.h1 = styled(Heading)``
@@ -19,7 +19,8 @@ Heading.h1.defaultProps = {
   as: 'h1',
   fontSize: 6,
   fontWeight: 3,
-  my: 3,
+  textAlign: 'left',
+  my: 3
 }
 
 Heading.h2 = styled(Heading)``
@@ -27,35 +28,40 @@ Heading.h2.defaultProps = {
   as: 'h2',
   fontSize: 5,
   fontWeight: 3,
-  my: 3,
+  textAlign: 'left',
+  my: 3
 }
 Heading.h3 = styled(Heading)``
 Heading.h3.defaultProps = {
   as: 'h3',
   fontSize: 4,
   fontWeight: 3,
-  my: 3,
+  textAlign: 'left',
+  my: 3
 }
 Heading.h4 = styled(Heading)``
 Heading.h4.defaultProps = {
   as: 'h4',
   fontSize: 3,
   fontWeight: 3,
-  my: 3,
+  textAlign: 'left',
+  my: 3
 }
 Heading.h5 = styled(Heading)``
 Heading.h5.defaultProps = {
   as: 'h5',
   fontSize: 2,
   fontWeight: 3,
-  my: 3,
+  textAlign: 'left',
+  my: 3
 }
 Heading.h6 = styled(Heading)``
 Heading.h6.defaultProps = {
   as: 'h6',
   fontSize: 1,
   fontWeight: 3,
-  my: 3,
+  textAlign: 'left',
+  my: 3
 }
 
 export default Heading
