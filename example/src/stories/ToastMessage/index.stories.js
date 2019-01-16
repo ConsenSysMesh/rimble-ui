72bf1de755023a4c877bb01a10629a06150302db
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-01-16 16:26:40 -0700

    update example

--- example/src/stories/ToastMessage/index.stories.js
+++ example/src/stories/ToastMessage/index.stories.js
@@ -4,26 +4,82 @@ import { withDocs } from 'storybook-readme';
 
 import MyREADME from './README.md';
 
-import { ToastMessage } from 'rimble-ui'
+import {
+  Flex,
+  Box,
+  ToastMessage
+} from 'rimble-ui'
 
 storiesOf('ToastMessage', module)
   .addDecorator(withDocs(MyREADME))
   .add('ToastMessage', () => (
     <div>
-      <ToastMessage>
-        Transaction confirmed
-      </ToastMessage>
-      <ToastMessage variant="success" my={2}>
-        Transaction confirmed
-      </ToastMessage>
-      <ToastMessage variant="warning" my={2}>
-        Transaction confirmed
-      </ToastMessage>
-      <ToastMessage variant="danger" my={2}>
-        Transaction confirmed
-      </ToastMessage>
-      <ToastMessage variant="info" my={2}>
-        Transaction confirmed
-      </ToastMessage>
+      <Flex>
+        <Box width={'50%'} mr={4}>
+          <ToastMessage my={3} />
+
+          <ToastMessage
+            secondaryMessage={'Lorem ipsum dolor sit.'}
+            my={3}
+          />
+          <ToastMessage
+            icon={'InfoOutline'}
+            my={3}
+          />
+          <ToastMessage
+            secondaryMessage={'Lorem ipsum dolor sit.'}
+            icon={'InfoOutline'}
+            my={3}
+          />
+
+          <hr/>
+
+          <ToastMessage
+            my={3}
+            icon={'Sync'}
+            message={'Processing Payment'}
+            actionText={'View Details'}
+          />
+          <ToastMessage
+            my={3}
+            icon={'Check'}
+            message={'Payment Confirmed'}
+            secondaryMessage={'6efd...5909'}
+            actionText={'View Details'}
+          />
+          <ToastMessage
+            my={3}
+            icon={'Warning'}
+            message={'Payment Failed'}
+            secondaryMessage={'6efd...5909'}
+            actionText={'View Details'}
+          />
+
+        </Box>
+        <Box width={'50%'}>
+          <ToastMessage variant={'dark'} my={3}/>
+          <ToastMessage
+            secondaryMessage={'Lorem ipsum dolor sit.'}
+            variant={'dark'}
+            my={3}
+          />
+          <ToastMessage
+            icon={'InfoOutline'}
+            variant={'dark'}
+            my={3}
+          />
+          <ToastMessage
+            secondaryMessage={'Lorem ipsum dolor sit.'}
+            icon={'InfoOutline'}
+            variant={'dark'}
+            my={3}
+          />
+        </Box>
+      </Flex>
+
+      <ToastMessage.Container>
+
+      </ToastMessage.Container>
+
     </div>
   ))
