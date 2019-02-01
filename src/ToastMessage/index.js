8046b911e1bdbc0c95adbb09927f6e561419cb4c
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-01-31 22:27:52 -0700

    move AnimatedIconProcessing to own file

--- src/ToastMessage/index.js
+++ src/ToastMessage/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import styled, { keyframes } from 'styled-components'
 import { timingFunctions, ellipsis } from 'polished'
 
@@ -8,64 +8,10 @@ import Text from '../Text'
 import Link from '../Link'
 import TextButton from '../TextButton'
 
+import AnimatedIconProcessing from './AnimatedIconProcessing.js'
 import { ReactComponent as IconPositive } from './icon-positive.svg'
 import { ReactComponent as IconNegative } from './icon-negative.svg'
-import { ReactComponent as IconProcessing } from './icon-processing.svg'
 
-
-const flicker = keyframes`
-  0% {
-    opacity: 0.1;
-    transform: translateY(-1px);
-  }
-  20% {
-    opacity: 0.6;
-  }
-  40% {
-    opacity: 0.9;
-    transform: translateY(1px);
-  }
-  60% {
-    opacity: 0.5;
-    transform: translateY(2px);
-  }
-  100% {
-    opacity: 0;
-    transform: translateY(3px);
-  }
-`
-
-const WrappedIconProcessing = (props) => (
-  <IconProcessing {...props} />
-)
-
-const AnimatedIconProcessing = styled(WrappedIconProcessing).attrs({
-  seed: Math.floor(1000 + Math.random() * 7000),
-  seeda: Math.floor((Date.now().toString().substring(2,6)) + Math.random() * 9000).toString().substring(0,4),
-  seedb: Math.floor((Date.now().toString().substring(0,4)) + Math.random() * 4500).toString().substring(0,4)
-})`
-  & {
-    display: block;
-  }
-  > * {
-    animation-name: ${flicker};
-    animation-duration: ${props => props.seed + 'ms'};
-    animation-timing-function: ease;
-    animation-delay: 0s;
-    animation-iteration-count: infinite;
-  }
-  > *:nth-child(4n+0) {
-    animation-duration: ${props => props.seeda + 'ms'};
-    animation-delay: ${props => props.seeda + 'ms'};
-  }
-  > *:nth-child(2n+0) {
-    animation-duration: ${props => props.seedb + 'ms'};
-  }
-  > *:nth-child(3n+0) {
-    animation-duration: ${props => props.seeda + 'ms'};
-    animation-delay: ${props => props.seedb + 'ms'};
-  }
-`
 
 const animInKeyframes = keyframes`
   from {
