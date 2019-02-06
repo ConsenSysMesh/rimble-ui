afb5746510ac13dd2bfef38cfaba4a29a4db941c
ryancreatescopy <40891631+ryancreatescopy@users.noreply.github.com>
2019-02-06 08:37:33 +0000

    TEST - changed button copy

--- example/src/stories/ToastMessage/index.stories.js
+++ example/src/stories/ToastMessage/index.stories.js
@@ -84,36 +84,36 @@ storiesOf('ToastMessage', module)
       <Button
         mb={3}
         onClick={(e) => window.toastProvider.addMessage('[Processing payment...]', {
-            secondaryMessage: 'test',
+            secondaryMessage: '',
             actionHref: 'https://etherscan.io/tx/0xcbc921418c360b03b96585ae16f906cbd48c8d6c2cc7b82c6db430390a9fcfed',
             actionText: 'View on Etherscan',
             variant: 'processing',
           })
         }
       >
-        "Preview addMessage.processing"
+        'Preview "addMessage.processing"'
       </Button>
       <br/>
       <Button
         mb={3}
-        onClick={(e) => window.toastProvider.addMessage('[ETH sent ]', {
-            secondaryMessage: (Date.now()),
+        onClick={(e) => window.toastProvider.addMessage('[ETH sent]', {
+            secondaryMessage: '[You have 1.03 Ether remaining]',
             variant: 'success',
           })
         }
       >
-        "addMessage.success"
+        'Preview "addMessage.success"'
       </Button>
       <br/>
       <Button
         mb={3}
-        onClick={(e) => window.toastProvider.addMessage('[Payment failed ]', {
-            secondaryMessage: (Date.now()),
+        onClick={(e) => window.toastProvider.addMessage('[Payment failed]', {
+            secondaryMessage: 'Make sure you have enough Ether and try again',
             variant: 'failure',
           })
         }
       >
-        "addMessage.failure"
+        'Preview "addMessage.failure"'
       </Button>
       <br/>
       <Button
