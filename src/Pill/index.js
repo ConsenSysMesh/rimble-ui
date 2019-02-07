14f736914b693a22fa50445a800dcf80046655e7
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-02-07 00:15:26 -0700

    add Pill element

--- src/Pill/index.js
+++ src/Pill/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+import { themeGet } from 'styled-system'
+import { tint } from 'polished'
 
-class Pill extends Component {
+import Box from '../Box'
 
-  render() {
-    return (
-      <div></div>
-    );
+const Pill = styled(Box).attrs({
+  tintLevel: props => (props.selected ? '0.75' : '0.88')
+})`
+  & {
+    cursor: pointer;
+    border: ${ props => (props.selected ? '1px solid' : 'none') };
+    border-color: ${ props => (props.selected ? props.color : 'none') };
+    background-color: ${ props => tint(props.tintLevel, themeGet( ('colors.' + props.color), props.color)(props) ) };
   }
+  &:hover {
+    border-color: transparent;
+    background-color: ${ props => tint(0.8, themeGet( ('colors.' + props.color), props.color)(props) ) };
+  }
+`
 
+Pill.defaultProps = {
+  color: 'dark-gray',
+  px: 3,
+  py: 1,
+  fontSize: 1,
+  height: 2,
+  lineHeight: 'solid',
+  borderRadius: 3,
+  display: 'inline-flex',
+  alignItems: 'center',
+  selected: false
 }
 
 export default Pill;
