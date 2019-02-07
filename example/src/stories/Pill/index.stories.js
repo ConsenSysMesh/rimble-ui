ef7a0e21ba71e270629a2096b08961363db4fd00
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-02-07 00:15:43 -0700

    update story

--- example/src/stories/Pill/index.stories.js
+++ example/src/stories/Pill/index.stories.js
@@ -9,5 +9,22 @@ import { Pill } from 'rimble-ui'
 storiesOf('Pill', module)
   .addDecorator(withDocs(MyREADME))
   .add('default', () => (
-    <Pill>{'TEST'}</Pill>
+    <div>
+      <Pill mr={'2'} mb={'3'}>{'Pending'}</Pill>
+      <Pill selected={true} mr={'2'} mb={'3'}>{'Pending'}</Pill>
+
+      <br/>
+      <Pill color={'primary'} mr={'2'} mb={'3'}>{'Pending'}</Pill>
+      <Pill selected={true} color={'primary'} mr={'2'} mb={'3'}>{'Pending'}</Pill>
+
+      <br/>
+      <Pill color={'red'} mr={'2'} mb={'3'}>{'Pending'}</Pill>
+      <Pill selected={true} color={'red'} mr={'2'} mb={'3'}>{'Pending'}</Pill>
+
+      <br/>
+      <Pill color={'blue'} mr={'2'} mb={'3'}>{'Pending'}</Pill>
+      <Pill selected={true} color={'blue'} mr={'2'} mb={'3'}>{'Pending'}</Pill>
+
+
+    </div>
   ))
