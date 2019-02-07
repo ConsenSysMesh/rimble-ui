f22cf1da4210b1e5258d1d13107282b97e41de8a
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-02-07 16:05:35 -0700

    add Story example, wip

--- example/src/stories/Modal/index.stories.js
+++ example/src/stories/Modal/index.stories.js
@@ -1,15 +1,85 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react'
 import { withDocs } from 'storybook-readme';
 
 import MyREADME from './README.md';
 
-import { Modal } from 'rimble-ui'
+import {
+  Modal,
+  Card,
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Button,
+  TextButton,
+  OutlineButton
+} from 'rimble-ui'
+
+class YourModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: true
+    }
+  }
+
+  closeModal = (e) => {
+    e.preventDefault()
+    this.setState((state, props) => ({
+      isOpen: false
+    }))
+  }
+
+  openModal = (e) => {
+    e.preventDefault()
+    this.setState((state, props) => ({
+      isOpen: true
+    }))
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Button onClick={this.openModal}>Open Modal</Button>
+        
+        <Modal isOpen={this.state.isOpen}>
+          <Card width={'420px'} p={0}>
+            <TextButton
+              icononly
+              icon={'Close'}
+              color={'moon-gray'}
+              position={'absolute'}
+              top={0}
+              right={0}
+              mt={3}
+              mr={3}
+              onClick={this.closeModal}
+            />
+            <Box p={4} mb={3}>
+              <Heading.h3>Confirm that thing</Heading.h3>
+              <Text>
+                Do you reaaaaaally want to do that thing you just said that you wanted to do?
+              </Text>
+            </Box>
+            <Flex px={4} py={3} borderTop={1} borderColor={'#E8E8E8'} justifyContent={'flex-end'}>
+              <OutlineButton>Cancel</OutlineButton>
+              <Button ml={3}>Confirm</Button>
+            </Flex>
+          </Card>
+        </Modal>
+      </React.Fragment>
+    );
+  }
+
+}
+
 
 storiesOf('Modal', module)
   .addDecorator(withDocs(MyREADME))
-  .add('Default', () => (
-    <div>
-      <Modal></Modal>
-    </div>
-  ))
+  .add('Default', () => {
+
+    return (
+      <YourModal></YourModal>
+    )
+  })
