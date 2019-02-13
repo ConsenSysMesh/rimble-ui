15b394384cd5653641aa063f99e5b529a8b5cc8a
Mike Lock <michael.lockwitz@consensys.net>
2019-02-13 13:48:46 -0700

    Wrapping all failing tests in ThemeProvider.

--- src/Input/Input.test.js
+++ src/Input/Input.test.js
@@ -1,12 +1,18 @@
 import Input from './'
+import ThemeProvider from '../ThemeProvider'
+import theme from '../theme'
 
 describe('Input component sanity', () => {
   it('has name', () => {
     expect(Input.displayName).toBe('Input')
   })
 
-  it('matches default snapshot', () => {
-    const component = render(<Input />)
+  it('matches themed snapshot', () => {
+    const component = render(
+      <ThemeProvider theme={theme}>
+        <Input />
+      </ThemeProvider>
+    )
     expect(component).toMatchSnapshot()
   })
 })
--- src/Radio/Radio.test.js
+++ src/Radio/Radio.test.js
@@ -1,12 +1,17 @@
 import Radio from './'
+import ThemeProvider from '../ThemeProvider'
+import theme from '../theme'
 
 describe('Radio component sanity', () => {
   it('has name', () => {
     expect(Radio.displayName).toBe('Radio')
   })
 
-  it('matches default snapshot', () => {
-    const component = render(<Radio />)
+  it('matches themed snapshot', () => {
+    const component = render(
+      <ThemeProvider theme={theme}>
+        <Radio />
+      </ThemeProvider>)
     expect(component).toMatchSnapshot()
   })
 })
