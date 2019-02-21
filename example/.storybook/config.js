b97ab715cee585ff8ccc104899e910918d4a3a75
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-02-20 20:27:36 -0700

    add more themes to storybook

--- example/.storybook/config.js
+++ example/.storybook/config.js
@@ -1,8 +1,31 @@
 import { configure, addDecorator } from '@storybook/react'
 import { withOptions } from '@storybook/addon-options';
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 
-import React from 'react';
-import { ThemeProvider } from 'rimble-ui'
+import { theme } from 'rimble-ui'
+
+const theme1 = {
+  ...theme,
+  name: 'Rimble'
+}
+const theme2 = {
+  ...theme,
+  name: 'ConsenSys',
+  colors: {
+    ...theme.colors,
+    primary: '#2c56dd'
+  }
+}
+const theme3 = {
+  ...theme,
+  name: 'Salmon',
+  colors: {
+    ...theme.colors,
+    primary: 'salmon'
+  }
+}
+
+const themes = [theme1, theme2, theme3];
 
 addDecorator(
   withOptions({
@@ -11,11 +34,7 @@ addDecorator(
   })
 )
 
-addDecorator((story) => (
-  <ThemeProvider>
-    {story()}
-  </ThemeProvider>
-))
+addDecorator(withThemesProvider(themes));
 
 function loadStories() {
   const req = require.context('../src/stories', true, /\.stories\.js$/)
