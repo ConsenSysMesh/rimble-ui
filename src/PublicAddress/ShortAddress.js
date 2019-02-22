a69e1c838572f9c29418571e5f039b7c1b74b031
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-02-21 17:54:15 -0700

    added copy/paste functionality and styles

--- src/PublicAddress/ShortAddress.js
+++ src/PublicAddress/ShortAddress.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+import ClipboardJS from 'clipboard'
 
-const addressSummary = (address, firstSegLength = 10, lastSegLength = 4, includeHex = true) => {
+import Box from '../Box'
+import Icon from '../Icon'
+import Pill from '../Pill'
+
+const addressSummary = (address, firstSegLength = 5, lastSegLength = 4, includeHex = true) => {
   if (!address) return ''
   // let checked = checksumAddress(address)
   // if (!includeHex) {
@@ -10,10 +16,46 @@ const addressSummary = (address, firstSegLength = 10, lastSegLength = 4, include
 }
 
 class ShortAddress extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false
+    }
+    this.myRef = React.createRef();
+    this.clipboard;
+  }
+
+  componentDidMount() {
+    this.clipboard = new ClipboardJS(this.myRef.current, {
+      target: () => (this.myRef.current),
+      text: () => (this.props.address)
+    })
+
+    this.clipboard.on('success', (e) => {
+      this.setState({copied: true})
+    })
+  }
+
+  componentWillUnmount() {
+    this.clipboard.destroy()
+  }
+
+  renderIcon() {
+    if (this.state.copied) {
+      return <Icon name={'Check'} />
+    } else {
+      return <Icon name={'ContentCopy'} size={'1rem'} />
+    }
+  }
 
   render() {
     return (
-      <div>{addressSummary(this.props.address)}</div>
+      <Pill ref={this.myRef} color={'primary'}>
+        {addressSummary(this.props.address)}
+        <Box ml={2} width={'20px'} display={'flex'} justifyContent={'flex-end'}>
+          {this.renderIcon()}
+        </Box>
+      </Pill>
     );
   }
 
