b89e497beffd20178cfbf3740b3d585576ae13ca
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-02-28 17:30:23 -0700

    added Form stories, wip

--- example/src/stories/Input/input.stories.js
+++ example/src/stories/Input/input.stories.js
@@ -6,7 +6,15 @@ import MyREADME from './README.md';
 
 import { Input } from 'rimble-ui'
 
-storiesOf('Form', module)
+import { Button } from 'rimble-ui'
+import { Field } from 'rimble-ui'
+import { Checkbox } from 'rimble-ui'
+import { Image } from 'rimble-ui'
+import { Flex } from 'rimble-ui'
+
+import { Card } from 'rimble-ui'
+
+storiesOf('Form/Input', module)
   .addDecorator(withDocs(MyREADME))
   .add('Input', () => (
     <div>
@@ -29,3 +37,50 @@ storiesOf('Form', module)
   )).add('url', () => (
     <Input type='url'/>
   ))
+
+storiesOf('Form/Validation', module)
+.add('Form', () => (
+  <div>
+    <form>
+      <Field for="choose" label='Would you prefer a banana or cherry?'>
+        <Input id="choose" name="i_like" required />
+      </Field>
+      <Field label='Would you prefer a red or blue?'>
+        <Input id="choose" name="i_like" />
+      </Field>
+
+      <Flex alignItems={'center'} mb={3}>
+        <Checkbox label="Hey, 'check' me out " my={2} />
+        <Image
+          size='1rem'
+          src='http://orig14.deviantart.net/c6aa/f/2014/303/5/d/awesome_face_by_zorogleb-d84ne3x.jpg'
+        />
+      </Flex>
+
+      <Button type="submit">Submit</Button>
+
+      {/* <label for="choose">
+        Would you prefer a banana or cherry?
+      </label>
+
+      <Input id="choose" name="i_like" required />
+      <Button>Submit</Button> */}
+    </form>
+  </div>
+))
+.add('Form with Card', () => (
+  <div>
+    <Card>
+      <form action="">
+        <Field label='Email' width={1}>
+          <Input required width={1}/>
+        </Field>
+        <Field label='Password' width={1}>
+          <Input required width={1}/>
+        </Field>
+        <Checkbox label="Remember me?" mb={3} />
+        <Button type="submit" width={1}>Sign Up</Button>
+      </form>
+    </Card>
+  </div>
+))
