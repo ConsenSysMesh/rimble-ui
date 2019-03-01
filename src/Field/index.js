841eb0e26416ede30ec164f4d10505309f9737d3
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-02-28 17:30:13 -0700

    tried to add requried to required fields, wip

--- src/Field/index.js
+++ src/Field/index.js
@@ -1,16 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Box from '../Box'
 import Text from '../Text'
 
-const Field = ({ children, label, ...props }) => (
-  <Box as='label' display='flex' flexDirection='column' alignItems='flex-start' {...props}>
-    <Text fontSize={1} fontWeight={3} mb={2}>
-      {label}
-    </Text>
-    {children}
-  </Box>
-)
+const WithValidationIcon = styled(Box)`
+  & {
+    
+  }
+`
+
+const RequiredLabel = styled(Text)`
+  &:after {
+    content: ' (required)'
+  }
+`
+
+const Field = ({ children, label, ...props }) => {
+  console.log(children.props.required)
+  return (
+    <Box
+      as='label'
+      display='flex'
+      flexDirection='column'
+      alignItems='flex-start'
+      mb={3}
+      {...props}
+    >
+
+      {!children.props.required && <Text fontSize={1} fontWeight={3} mb={2}>{label}</Text> }
+      {children.props.required && <RequiredLabel fontSize={1} fontWeight={3} mb={2}>{label}</RequiredLabel> }
+      {children}
+    </Box>
+  )
+}
 
 Field.displayName = 'Field'
 
