95bb5d5b12f11666f3c636df4f030912c02f762c
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-03-06 17:01:50 -0700

    updated stories with new Form component

--- example/src/stories/Input/input.stories.js
+++ example/src/stories/Input/input.stories.js
@@ -4,74 +4,83 @@ import { withDocs } from 'storybook-readme';
 
 import MyREADME from './README.md';
 
-import { Input } from 'rimble-ui'
-
-import { Button } from 'rimble-ui'
-import { Field } from 'rimble-ui'
-import { Checkbox } from 'rimble-ui'
-import { Image } from 'rimble-ui'
-import { Flex } from 'rimble-ui'
-import { Card } from 'rimble-ui'
+import {
+  Flex,
+  Card,
+  Image,
+  Button,
+  Form,
+} from 'rimble-ui'
 
 class MyCustomForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {validated: false};
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({ validated: true });
+  }
+
+  handleValidation = (e) => {
+    e.target.parentNode.classList.add('was-validated')
+  }
 
   render() {
     return (
-      <form onSubmit={(event) => event.preventDefault()}>
-        <Field label='Email' width={1}>
-          <Input type="email" required width={1}/>
-        </Field>
-        <Field label='Password' width={1}>
-          <Input type="password" required width={1}/>
-        </Field>
-        <Checkbox label="Remember me?" mb={3} />
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Field label='Email' width={1}>
+          <Form.Input type="email" required width={1} onChange={this.handleValidation} />
+        </Form.Field>
+        <Form.Field validated={this.state.validated} label='Password' width={1}>
+          <Form.Input type="password" required width={1} onChange={this.handleValidation}/>
+        </Form.Field>
+        <Form.Check label="Remember me?" mb={3} onChange={this.handleValidation} />
         <Button type="submit" width={1}>Sign Up</Button>
-      </form>
+      </Form>
     );
   }
 
 }
 
-
-
-
 storiesOf('Form/Input', module)
   .addDecorator(withDocs(MyREADME))
   .add('Input', () => (
     <div>
-      <Input type='text' placeholder='Lorem ipsum dolor sit.'/>
-      <Input type='text' placeholder='Lorem ipsum dolor sit.' disabled/>
+      <Form.Input type='text' placeholder='Lorem ipsum dolor sit.'/>
+      <Form.Input type='text' placeholder='Lorem ipsum dolor sit.' disabled/>
     </div>
   )).add('email', () => (
     <div>
-      <Input type='email' name='email' id='email' placeholder='you@example.com'/>
-      <Input type='email' name='email' id='email' placeholder='you@example.com' disabled/>
+      <Form.Input type='email' name='email' id='email' placeholder='you@example.com'/>
+      <Form.Input type='email' name='email' id='email' placeholder='you@example.com' disabled/>
     </div>
   )).add('number', () => (
-    <Input type='number'/>
+    <Form.Input type='number'/>
   )).add('password', () => (
-    <Input type='password'/>
+    <Form.Input type='password'/>
   )).add('search', () => (
-    <Input type='search'/>
+    <Form.Input type='search'/>
   )).add('tel', () => (
-    <Input type='tel'/>
+    <Form.Input type='tel'/>
   )).add('url', () => (
-    <Input type='url'/>
+    <Form.Input type='url'/>
   ))
 
 storiesOf('Form/Validation', module)
 .add('Form', () => (
   <div>
-    <form onSubmit={(event) => event.preventDefault()}>
-      <Field for="choose" label='Would you prefer a banana or cherry?'>
-        <Input id="choose" name="i_like" required pattern="banana|cherry" />
-      </Field>
-      <Field label='Would you prefer a red or blue?'>
-        <Input id="choose" name="i_like" />
-      </Field>
+    <Form onSubmit={(event) => event.preventDefault()}>
+      <Form.Field for="choose" label='Would you prefer a banana or cherry?'>
+        <Form.Input id="choose" name="i_like" required pattern="banana|cherry" />
+      </Form.Field>
+      <Form.Field label='Would you prefer a red or blue?'>
+        <Form.Input id="choose" name="i_like" />
+      </Form.Field>
 
       <Flex alignItems={'center'} mb={3}>
-        <Checkbox label="Hey, 'check' me out " my={2} />
+        <Form.Check label="Hey, 'check' me out " my={2} />
         <Image
           size='1rem'
           src='http://orig14.deviantart.net/c6aa/f/2014/303/5/d/awesome_face_by_zorogleb-d84ne3x.jpg'
@@ -79,14 +88,7 @@ storiesOf('Form/Validation', module)
       </Flex>
 
       <Button type="submit">Submit</Button>
-
-      {/* <label for="choose">
-        Would you prefer a banana or cherry?
-      </label>
-
-      <Input id="choose" name="i_like" required />
-      <Button>Submit</Button> */}
-    </form>
+    </Form>
   </div>
 ))
 .add('Form with Card', () => (
