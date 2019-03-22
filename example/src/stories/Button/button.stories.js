475f59ba698449a5e96df584526d80f757f5aac2
ryancreatescopy <40891631+ryancreatescopy@users.noreply.github.com>
2019-03-22 14:26:15 +0000

    Updating button structure

--- example/src/stories/Button/button.stories.js
+++ example/src/stories/Button/button.stories.js
@@ -16,12 +16,10 @@ import {
 
 storiesOf('Button', module)
   // .addDecorator(withDocs(UsageDoc))
-  .add('default', () => (
+  .add('Documentation', withDocs(UsageDoc, ()=> (
     <div>
       <Button>Click Me</Button>
     </div>
-  ))
-  .add('sizes', () => (
     <div>
       <Button size='small' mb={3}>Small Button</Button>
       <br/>
@@ -31,11 +29,9 @@ storiesOf('Button', module)
       <br/>
       <Button fullWidth>Full-width Button</Button>
     </div>
-  ))
-  .add('Disabled', () => (
-    <Button disabled >disabled Button</Button>
-  ))
-  .add('Icon', () => (
+    <div>
+      <Button disabled >disabled Button</Button>
+    </div>
     <div>
       <Button icon="Check" mb="3">Click me!</Button>
       <br/>
@@ -45,8 +41,6 @@ storiesOf('Button', module)
       <OutlineButton icon='MoreHoriz' icononly mb="3" mr={2}/>
       <TextButton icon='MoreHoriz' icononly  mb="3" />
     </div>
-  ))
-  .add('Loader', () => (
     <div>
       <Button>
         <Loader color='white'/>
@@ -57,4 +51,34 @@ storiesOf('Button', module)
     <div>
       <Button>Click Me</Button>
     </div>
+   <div>
+      <Button>Click Me</Button>
+    </div>
+    <div>
+      <Button size='small' mb={3}>Small Button</Button>
+      <br/>
+      <Button size='medium' mb={3}>Medium Button</Button>
+      <br/>
+      <Button size='large' mb={3}>Large Button</Button>
+      <br/>
+      <Button fullWidth>Full-width Button</Button>
+    </div>
+    <div>
+      <Button disabled >disabled Button</Button>
+    </div>
+    <div>
+      <Button icon="Check" mb="3">Click me!</Button>
+      <br/>
+      <Button icon="Check" iconpos="right"  mb="3">Click me!</Button>
+      <br/>
+      <Button icon='MoreHoriz' icononly mb="3" mr={2}/>
+      <OutlineButton icon='MoreHoriz' icononly mb="3" mr={2}/>
+      <TextButton icon='MoreHoriz' icononly  mb="3" />
+    </div>
+    <div>
+      <Button>
+        <Loader color='white'/>
+      </Button>
+    </div>
+
   )))
