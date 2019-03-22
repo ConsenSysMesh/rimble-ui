5b34ea1a1ec28ccaf822c59c519586b29534164b
ryancreatescopy <40891631+ryancreatescopy@users.noreply.github.com>
2019-03-22 14:02:51 +0000

    Seeing if I can get welcome to appear in preview

--- example/src/stories/1-Index/index.stories.js
+++ example/src/stories/1-Index/index.stories.js
@@ -4,7 +4,9 @@ import { doc } from 'storybook-readme';
 
 import themingREADME from './theming-README.md';
 import installREADME from './install-README.md';
+import welcomeREADME from './install-README.md';
 
 storiesOf('Getting Started', module)
   .add('Installation', doc(installREADME))
   .add('Theming', doc(themingREADME))
+  .add('Welcome', doc(welcomeREADME))
