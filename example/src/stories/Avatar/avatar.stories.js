91316c1761675d430d286b0deb77c7960890c330
Ryan Cordell <ryan.cordell@consensys.net>
2019-03-26 15:10:58 +0000

    Update avatar.stories.js

--- example/src/stories/Avatar/avatar.stories.js
+++ example/src/stories/Avatar/avatar.stories.js
@@ -28,15 +28,17 @@ storiesOf('Avatar', module)
         <Avatar mb={3} src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg' />
       </Box>
       <Box bg={'#f7f7f7'} my={2} p={2} radius={3}>
-        <code>{'<Avatar src= imagelink />'}</code>
-      </Box>
+  <code>
+    { '<Avatar src=\'https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg\' />' }
+  </code>
+</Box>
       <br />
       <Box>
         <Heading.h4>Custom size</Heading.h4>
       <Avatar mb={3} size='100px' src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg' />
       </Box>
       <Box bg={'#f7f7f7'} my={2} p={2} radius={3}>
-        <code>{'<Avatar size= 100px src= https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg />'}</code>
+        <code>{'<Avatar size=\'100px\' src=\'https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg\' />'}</code>
       </Box>
       <br />
       <Box>
@@ -48,7 +50,7 @@ storiesOf('Avatar', module)
       />
       </Box>
       <Box bg={'#f7f7f7'} my={2} p={2} radius={3}>
-        <code>{'<Avatar size= large src= https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg />'}</code>
+        <code>{'<Avatar size=\'large\' src=\'https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg\' />'}</code>
       </Box>
       <br />
       <Box>
@@ -60,7 +62,7 @@ storiesOf('Avatar', module)
       />
       </Box>
       <Box bg={'#f7f7f7'} my={2} p={2} radius={3}>
-        <code>{'<Avatar size= medium src= https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg />'}</code>
+        <code>{'<Avatar size="medium" src="https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg" />'}</code>
       </Box>
       <br />
       <Box>
@@ -72,7 +74,7 @@ storiesOf('Avatar', module)
       />
       </Box>
       <Box bg={'#f7f7f7'} my={2} p={2} radius={3}>
-        <code>{'<Avatar size= small src= https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg />'}</code>
+        <code>{'<Avatar size=\'small\' src=\'https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg\' />'}</code>
       </Box>
       <br />
     </Box>
