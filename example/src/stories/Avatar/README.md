c9378fb75dc382951fa2ca70f330ae912d99cc51
Ryan Cordell <ryan.cordell@consensys.net>
2019-03-26 09:27:01 +0000

    Avatar updated

--- example/src/stories/Avatar/README.md
+++ example/src/stories/Avatar/README.md
@@ -1,38 +1,18 @@
 # Avatar
 A basic avatar component.
 
-<!-- STORY -->
-
-### Usage
+**Import component**
 ```jsx
 import { Avatar } from 'rimble-ui'
 ```
 
+<!-- STORY -->
+
+
+
 <!-- Avatar example here -->
-```jsx
-<Avatar />
 
-<Avatar src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg' />
-
-<Avatar size='100px' src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg' />
-
-<Avatar
-  size='large'
-  src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg'
-/>
-
-<Avatar
-  size='medium'
-  src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg'
-/>
-
-<Avatar
-  size='small'
-  src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg'
-/>
-```
-
-### Props
+### Component props
 | Name   | Type   | Default | Description                                 |
 | ------ | ------ | ------- | ------------------------------------------- |
 | `size` | string |         | Sets avatar size to small, medium, or large |
--- example/src/stories/Avatar/avatar.stories.js
+++ example/src/stories/Avatar/avatar.stories.js
@@ -4,27 +4,76 @@ import { withDocs } from 'storybook-readme';
 
 import MyREADME from './README.md';
 
-import { Avatar } from 'rimble-ui'
+import { Avatar, Box, Heading, Text } from 'rimble-ui'
 
 storiesOf('Avatar', module)
   .addDecorator(withDocs(MyREADME))
   .add('Default', () => (
-    <div>
-      <Avatar />
-      <Avatar src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg' />
-
-      <Avatar size='100px' src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg' />
+    <Box textAlign={'left'}>
+      <Box>
+        <Heading.h2>Avatar examples</Heading.h2>
+        <Text>Different ways to use the Avatar component</Text>
+        <br />
+      </Box>
+      <Box>
+        <Heading.h4>Standard avatar</Heading.h4>
+        <Avatar mb={3}/>
+      </Box>
+      <Box bg={'#f7f7f7'} my={2} p={2} radius={3}>
+        <code>{'<Avatar />'}</code>
+      </Box>
+      <br />
+      <Box>
+        <Heading.h4>Standard with image</Heading.h4>
+        <Avatar mb={3} src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg' />
+      </Box>
+      <Box bg={'#f7f7f7'} my={2} p={2} radius={3}>
+        <code>{'<Avatar src= imagelink />'}</code>
+      </Box>
+      <br />
+      <Box>
+        <Heading.h4>Custom size</Heading.h4>
+      <Avatar mb={3} size='100px' src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg' />
+      </Box>
+      <Box bg={'#f7f7f7'} my={2} p={2} radius={3}>
+        <code>{'<Avatar size= 100px src= https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg />'}</code>
+      </Box>
+      <br />
+      <Box>
+        <Heading.h4>Large avatar</Heading.h4>
       <Avatar
+        mb={3}
         size='large'
         src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg'
       />
+      </Box>
+      <Box bg={'#f7f7f7'} my={2} p={2} radius={3}>
+        <code>{'<Avatar size= large src= https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg />'}</code>
+      </Box>
+      <br />
+      <Box>
+        <Heading.h4>Medium avatar</Heading.h4>
       <Avatar
+        mb={3}
         size='medium'
         src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg'
       />
+      </Box>
+      <Box bg={'#f7f7f7'} my={2} p={2} radius={3}>
+        <code>{'<Avatar size= medium src= https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg />'}</code>
+      </Box>
+      <br />
+      <Box>
+      <Heading.h4>Small avatar</Heading.h4>
       <Avatar
+        mb={3}
         size='small'
         src='https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg'
       />
-    </div>
+      </Box>
+      <Box bg={'#f7f7f7'} my={2} p={2} radius={3}>
+        <code>{'<Avatar size= small src= https://miro.medium.com/fit/c/240/240/1*1ByDBR2I219gNx76GLSJgg.jpeg />'}</code>
+      </Box>
+      <br />
+    </Box>
   ))
