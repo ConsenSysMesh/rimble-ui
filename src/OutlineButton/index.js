f03352159042771b13cd90dc5af665c691b21cac
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-04-05 15:50:01 +0200

    changed background from white to transparent
    Discourage users from using OutlineButton as a white button on dark backgrounds. The background and text color of the Button can be changed.

--- src/OutlineButton/index.js
+++ src/OutlineButton/index.js
@@ -12,14 +12,14 @@ const OutlineButton = styled(Button).attrs(props => ({
   primarycolor: themeGet('colors.primary', '#000')(props),
 }))`
   & {
-    background: white;
+    background: transparent;
   }
-  &:hover {
+  &&:hover {
     color: ${props => tint(0.2, props.primarycolor)};
     border-color: ${props => tint(0.2, props.primarycolor)};
-    background-color: #fff;
+    background-color: transparent;
   }
-  &:active {
+  &&:active {
     color: ${props => shade(0.2, props.primarycolor)};
     border-color: ${props => shade(0.2, props.primarycolor)};
     background-color: ${props => tint(0.9, props.primarycolor)};
@@ -29,7 +29,7 @@ const OutlineButton = styled(Button).attrs(props => ({
 OutlineButton.defaultProps = {
   theme: defaultTheme,
   color: 'primary',
-  bg: 'white',
+  bg: 'transparent',
   fontWeight: 3,
   fontFamily: 'sansSerif',
   height: '48px',
