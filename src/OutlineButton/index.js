5de47ca7469d708fd580a135d60d95bf4a9d67ad
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-04-05 15:47:51 +0200

    Revert "changed background to transparent instead of white"
    This reverts commit 38b56193f151e67998e219e533e0a7d0a27cc953.

--- src/OutlineButton/index.js
+++ src/OutlineButton/index.js
@@ -12,14 +12,14 @@ const OutlineButton = styled(Button).attrs(props => ({
   primarycolor: themeGet('colors.primary', '#000')(props),
 }))`
   & {
-    background: transparent;
+    background: white;
   }
-  &&:hover {
+  &:hover {
     color: ${props => tint(0.2, props.primarycolor)};
     border-color: ${props => tint(0.2, props.primarycolor)};
-    background-color: transparent;
+    background-color: #fff;
   }
-  &&:active {
+  &:active {
     color: ${props => shade(0.2, props.primarycolor)};
     border-color: ${props => shade(0.2, props.primarycolor)};
     background-color: ${props => tint(0.9, props.primarycolor)};
@@ -29,7 +29,7 @@ const OutlineButton = styled(Button).attrs(props => ({
 OutlineButton.defaultProps = {
   theme: defaultTheme,
   color: 'primary',
-  bg: 'transparent',
+  bg: 'white',
   fontWeight: 3,
   fontFamily: 'sansSerif',
   height: '48px',
