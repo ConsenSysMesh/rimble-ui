684ae430922e98522519d5872b6f85c12e2dcac9
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-04-17 19:25:00 +0200

    build ExampleCard component

--- example/src/components/ExampleCard.js
+++ example/src/components/ExampleCard.js
@@ -1,13 +1,70 @@
 import React, { Component } from 'react';
 
-class ExampleCard extends Component {
+import {
+  Box,
+  Flex,
+  Text,
+  Icon
+} from 'rimble-ui'
 
-  render() {
-    return (
-      <div></div>
-    );
+const colors = {
+  positive: '155, 66%, 45%',
+  negative: '8, 86%, 46%'
+}
+
+const ExampleCard = ({variant, ...props}) => {
+
+  let
+    colorPrimary = '0,0%,0%',
+    iconName = 'ThumbDown',
+    cardLabel = '{ missing variant }'
+
+  if (variant === 'positive') {
+    colorPrimary = colors.positive
+    iconName = 'Check'
+    cardLabel = 'Do'
+  } else if (variant === 'negative') {
+    colorPrimary = colors.negative
+    iconName = 'Close'
+    cardLabel = `Don't`
   }
 
+  return (
+    <Box
+      bg={'blacks.0'}
+      borderTop={`4px solid HSLA(${colorPrimary}, 1.00)`}
+    >
+      <Box
+        border={1}
+        borderColor={'blacks.3'}
+        borderWidth={'0 1px 1px 1px'}
+        p={4}
+      >
+        <Flex alignItems={'center'} mb={4}>
+          <Box
+            size={'3rem'}
+            bg={'white'}
+            border={`8px solid HSLA(${colorPrimary}, 0.40)`}
+            borderRadius={'100%'}
+          >
+            <Flex
+              size={'100%'}
+              border={`2px solid HSLA(${colorPrimary}, 1.00)`}
+              borderRadius={'100%'}
+              alignItems={'center'}
+              justifyContent={'center'}
+              >
+                <Icon name={iconName} size={'20px'} color={`HSLA(${colorPrimary}, 1.00)`} />
+            </Flex>
+          </Box>
+          <Text ml={3} fontSize={'18px'} fontWeight={3}>
+            {cardLabel}
+          </Text>
+        </Flex>
+        { props.children }
+      </Box>
+    </Box>
+  );
 }
 
 export default ExampleCard;
