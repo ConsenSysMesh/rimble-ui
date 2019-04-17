4c4fd876a304e383cf1baa035e9d4e709d836d20
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-04-17 15:10:53 +0200

    wip, updated stories

--- example/src/stories/Input/index.stories.js
+++ example/src/stories/Input/index.stories.js
@@ -3,8 +3,13 @@ import { storiesOf } from '@storybook/react';
 
 import { Input } from 'rimble-ui'
 
-
 storiesOf('Components/Input/', module)
   .add('<input type="file">', () => (
-    <Input type="file" id="file_upload"></Input>
+    <Input type="file" id="file_upload" />
+  ))
+  .add('<input type="file"> multiple', () => (
+    <Input type="file" id="file_upload" multiple />
+  ))
+  .add('<input type="file" variant="unstyled" >', () => (
+    <Input type="file" id="file_upload" variant={'unstyled'} />
   ))
