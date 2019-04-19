d8f53dbe7de0d061c119e71356b0bca4916cb32b
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-04-19 13:34:43 +0200

    added aria props

--- src/Flash/index.js
+++ src/Flash/index.js
@@ -4,15 +4,20 @@ import { variant } from 'styled-system';
 
 import defaultTheme from '../theme';
 import Text from '../Text';
+import Box from '../Box';
+
+let lastId = 0;
+const newID = (prefix = 'id') => `${prefix}${lastId++}`;
 
 const flashVariant = variant({
   key: 'messageStyle',
 });
 
-const StyledFlash = styled(Text)`
+const StyledFlash = styled(Box)`
   ${flashVariant}
 
   & {
+    position: relative;
   }
 
   a {
@@ -25,16 +30,42 @@ const StyledFlash = styled(Text)`
   }
 `;
 
-const Flash = ({ className, children, ...props }) => (
-  <StyledFlash className={className} {...props}>
-    {children}
-  </StyledFlash>
-);
+const Flash = ({ className, children, ...props }) => {
+  const id = newID('Flash');
+  const contentID = `${id}Content`;
+  let ariaRoleType = 'status';
+
+  switch (status) {
+    case 'warning':
+      ariaRoleType = 'alert';
+      break;
+    case 'danger':
+      ariaRoleType = 'alert';
+      break;
+    default:
+    // invalid status
+  }
+
+  return (
+    <StyledFlash
+      className={className}
+      tabIndex={0}
+      role={ariaRoleType}
+      aria-live="polite"
+      aria-describedby={contentID}
+      {...props}
+    >
+      <Text.span color={'inherit'} id={contentID}>
+        {children}
+      </Text.span>
+    </StyledFlash>
+  );
+};
 
 Flash.defaultProps = {
   theme: defaultTheme,
   variant: 'base',
-  fontFamily: 'sansSerif',
+  width: '1',
   p: '3',
   border: '1',
   borderRadius: 1,
