a95b6bf6f102130415d7d9420134168502fb7e22
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-05-03 18:37:05 +0200

    updated styles and structure for MetaMaskButton

--- src/MetaMaskButton/index.js
+++ src/MetaMaskButton/index.js
@@ -1,54 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import styled from 'styled-components';
 
-import Button from '../Button';
 import { ReactComponent as Fox } from './icon.svg';
+import Button, { StyledButton } from '../Button/BaseButton';
 
-const ProtoButton = ({ className, ...props }) => {
-  return (
-    <Button className={className} {...props}>
-      <Fox />
-      {props.children}
-    </Button>
-  );
-};
-
-const StyledButton = styled(ProtoButton)`
-  > span {
-    display: inherit;
-    align-items: center;
+const MMStyledButton = styled(StyledButton)`
+  & {
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    transition: all 0.15s ease;
   }
 
-  > span > svg:first-child {
-    display: block;
-    height: ${props => (props.size === 'small' ? '16px' : '24px')};
-    width: ${props => (props.size === 'small' ? '16px' : '24px')};
+  &:hover {
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  }
+
+  > svg.r-ff-icon:first-of-type {
+    height: ${props => (props.size === 'small' ? '1rem' : '1.5rem')};
+    width: ${props => (props.size === 'small' ? '1rem' : '1.5rem')};
     margin-right: 0.5rem;
+    margin-bottom: -2px;
   }
 `;
 
-const MetaMaskButtonSolid = styled(StyledButton)`
+const StyledButtonSolid = styled(MMStyledButton)`
   & {
-    color: white;
-    background: black;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    color: #fff;
+    background: #000;
   }
   &:hover {
-    background-color: rgba(0, 0, 0, 0.7);
+    background-color: #4c4c4c;
   }
   &:active {
-    background-color: rgba(0, 0, 0, 0.4);
+    background-color: #999;
   }
 `;
 
-const MetaMaskButtonOutline = styled(StyledButton)`
+const StyledButtonOutline = styled(MMStyledButton)`
   & {
-    color: #333333;
-    background: #ffffff;
-    border: 1px solid #cccccc;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    color: #333;
+    background: #fff;
+    border: 1px solid #ccc;
   }
   &:hover {
     background-color: #f3f2f2;
@@ -57,6 +49,38 @@ const MetaMaskButtonOutline = styled(StyledButton)`
     background-color: #e4e4e4;
   }
 `;
+
+const ButtonBody = ({ children, size }) => (
+  <React.Fragment>
+    <Fox className={'r-ff-icon'} size={size} />
+    {children}
+  </React.Fragment>
+);
+
+const MetaMaskButtonSolid = ({ className, children, ...props }) => {
+  return (
+    <StyledButtonSolid className={className} {...props}>
+      <ButtonBody children={children} size={props.size} />
+    </StyledButtonSolid>
+  );
+};
+
+const MetaMaskButtonOutline = ({ className, children, ...props }) => {
+  return (
+    <StyledButtonOutline className={className} {...props}>
+      <ButtonBody children={children} size={props.size} />
+    </StyledButtonOutline>
+  );
+};
+
+const defaultProps = {
+  ...Button.defaultProps,
+  borderRadius: 1,
+};
+
+MetaMaskButtonSolid.defaultProps = defaultProps;
+
+MetaMaskButtonOutline.defaultProps = defaultProps;
 
 const MetaMaskButton = MetaMaskButtonSolid;
 
