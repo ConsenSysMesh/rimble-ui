2cde5a713fb56f55468ddf3fa999e79b06abfda2
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-05-03 18:36:03 +0200

    updated styles and structure for TextButton

--- src/Button/TextButton.js
+++ src/Button/TextButton.js
@@ -1,38 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Button from './BaseButton';
+import Button, { StyledButton, ButtonBody } from './BaseButton';
 
 // text
-const StyledTextButton = styled(Button)`
+const StyledTextButton = styled(StyledButton)`
   & {
     color: var(--main-color);
     border: none;
     background: none;
     box-shadow: none;
   }
+
   &:hover {
     text-decoration: underline;
     background: inherit;
     box-shadow: inherit;
   }
+
   &:active {
     text-decoration: none;
   }
+
   &::before {
     display: none;
   }
 `;
 
-const TextButton = ({ className, children, ...props }) => {
+const TextButton = ({ className, children, icon, ...props }) => {
   return (
     <StyledTextButton className={className} {...props}>
-      {children}
+      <ButtonBody icon={icon} children={children} />
     </StyledTextButton>
   );
 };
 
 TextButton.defaultProps = {
+  ...Button.defaultProps,
   px: 2,
 };
 
