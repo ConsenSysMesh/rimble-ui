62d438e2a18efc6e5786a115cce4cf9f11aae06b
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-05-09 17:02:03 +0200

    added comments and simplified the button variable

--- src/Button/index.js
+++ src/Button/index.js
@@ -3,18 +3,21 @@ import TextButton from './TextButton';
 import OutlineButton from './OutlineButton';
 import SolidButton from './SolidButton';
 
-let Button = [];
+let Button;
 
-// solid
+// solid style
 Button = SolidButton;
 Button.displayName = 'Button';
 
+// outline style
 Button.Outline = OutlineButton;
 Button.Outline.displayName = 'Button.Outline';
 
+// text style
 Button.Text = TextButton;
 Button.Text.displayName = 'Button.Text';
 
+// base style
 Button.Base = BaseButton;
 Button.Base.displayName = 'Button.Base';
 
