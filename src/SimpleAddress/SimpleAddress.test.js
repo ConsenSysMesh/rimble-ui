4d625d2c220f81ff90c7a0d95046c85157b7e493
Jesse <murskyfuller@gmail.com>
2019-05-09 15:10:15 -0400

    Remove unused imports

--- src/SimpleAddress/SimpleAddress.test.js
+++ src/SimpleAddress/SimpleAddress.test.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { shallow, configure, render } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { render } from 'enzyme';
 
 import SimpleAddress from './';
 import ThemeProvider from '../ThemeProvider';
 import theme from '../theme';
 
 const testAddress = '0x99cb784f0429efd72wu39fn4256n8wud4e01c7d2';
-
-configure({ adapter: new Adapter() });
 
 describe('Simple Address component sanity', () => {
   it('has name', () => {
