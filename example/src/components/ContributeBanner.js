66b3ea78f5571988048d25bb5ed31fcb9bfe52a3
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-05-10 20:08:35 +0200

    fixed ContributeBanner

--- example/src/components/ContributeBanner.js
+++ example/src/components/ContributeBanner.js
@@ -3,18 +3,19 @@ import { Button, Box, Card, Text, Heading, Link } from 'rimble-ui';
 
 const ContributeBanner = () => (
   <Box my={4}>
-    <Text textAlign={'center'}>(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧</Text>
-    <Card color="white" bg="primary">
+    <Text textAlign={'center'} mb={4}>
+      {'(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧'}
+    </Text>
+    <Card color="white" bg="primary" border="none">
       <Heading.h2 color="white">Are we missing anything?</Heading.h2>
       <Text color="white" mb={4}>
         If you have any extra insight you'd like to add, please raise an issue
         in Github.
       </Text>
       <Button
-        color={'primary'}
-        hovercolor={'white'}
-        bg={'white'}
-        as={Link}
+        mainColor={'white'}
+        contrastColor={'primary'}
+        as={'a'}
         href="https://github.com/ConsenSys/rimble-ui/issues"
         title="GitHub Issues for Rimble UI"
         target="_blank"
