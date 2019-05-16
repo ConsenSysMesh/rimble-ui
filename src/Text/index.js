710339885cd9686b821a7b8353c4ee8964a2163f
Jesse <murskyfuller@gmail.com>
2019-05-16 14:46:34 -0400

    Spread Text component's default props on subtypes
    * Issue was theme was not getting through for static subtypes
    * Add theme to defaultProps means defaults are equal
    * Must set defaultProps on subtypes, otherwise styles are undefined
    * Can spread and modify if individual customization is needed in future

--- src/Text/index.js
+++ src/Text/index.js
@@ -61,33 +61,12 @@ Text.defaultProps = {
 Text.displayName = 'Text';
 
 Text.span = Text.withComponent('span');
-Text.span.defaultProps = {
-  color: 'copyColor',
-  fontFamily: 'sansSerif',
-  fontSize: 2,
-  fontWeight: 2,
-  lineHeight: 'copy',
-  textAlign: 'left',
-};
+Text.span.defaultProps = Text.defaultProps;
 
 Text.p = Text.withComponent('p');
-Text.p.defaultProps = {
-  color: 'copyColor',
-  fontFamily: 'sansSerif',
-  fontSize: 2,
-  fontWeight: 2,
-  lineHeight: 'copy',
-  textAlign: 'left',
-};
+Text.p.defaultProps = Text.defaultProps;
 
 Text.s = Text.withComponent('s');
-Text.s.defaultProps = {
-  color: 'copyColor',
-  fontFamily: 'sansSerif',
-  fontSize: 2,
-  fontWeight: 2,
-  lineHeight: 'copy',
-  textAlign: 'left',
-};
+Text.s.defaultProps = Text.defaultProps;
 
 export default Text;
