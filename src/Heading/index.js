111bdd50aef6389d36c108fc9ffeb4e20ac93316
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-05-23 18:09:31 +0200

    refactor Heading components

--- src/Heading/index.js
+++ src/Heading/index.js
@@ -1,84 +1,107 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+  compose,
+  color,
+  space,
+  fontFamily,
+  fontSize,
+  fontWeight,
+  lineHeight,
+  textAlign,
+  letterSpacing,
+} from 'styled-system';
 
-import Text from '../Text';
+import theme from '../theme';
 
-import defaultTheme from '../theme';
+const COMMON = compose(
+  color,
+  space
+);
 
-// body size + 6 headings
-const Heading = styled(Text)``;
+const TYPOGRAPHY = compose(
+  fontFamily,
+  fontSize,
+  fontWeight,
+  lineHeight,
+  textAlign,
+  letterSpacing
+);
+
+const defaultProps = {
+  theme,
+  fontFamily: 'sansSerif',
+  fontWeight: 3,
+  lineHeight: 'title',
+  m: 0,
+};
+
+// Heading
+const Heading = styled.h1`
+  ${TYPOGRAPHY}
+  ${COMMON}
+`;
+
 Heading.defaultProps = {
-  theme: defaultTheme,
+  ...defaultProps,
   as: 'h3',
-  fontFamily: 'sansSerif',
   fontSize: 4,
-  fontWeight: 3,
-  textAlign: 'left',
-  my: 3,
-};
-
-Heading.h1 = styled(Heading)``;
-Heading.h1.defaultProps = {
-  theme: defaultTheme,
-  as: 'h1',
-  fontFamily: 'sansSerif',
-  fontSize: 6,
-  fontWeight: 3,
-  textAlign: 'left',
-  my: 3,
-};
-
-Heading.h2 = styled(Heading)``;
-Heading.h2.defaultProps = {
-  theme: defaultTheme,
-  as: 'h2',
-  fontFamily: 'sansSerif',
-  fontSize: 5,
-  fontWeight: 3,
-  textAlign: 'left',
-  my: 3,
-};
-Heading.h3 = styled(Heading)``;
-Heading.h3.defaultProps = {
-  theme: defaultTheme,
-  as: 'h3',
-  fontFamily: 'sansSerif',
-  fontSize: 4,
-  fontWeight: 3,
-  textAlign: 'left',
-  my: 3,
-};
-Heading.h4 = styled(Heading)``;
-Heading.h4.defaultProps = {
-  theme: defaultTheme,
-  as: 'h4',
-  fontFamily: 'sansSerif',
-  fontSize: 3,
-  fontWeight: 3,
-  textAlign: 'left',
-  my: 3,
-};
-Heading.h5 = styled(Heading)``;
-Heading.h5.defaultProps = {
-  theme: defaultTheme,
-  as: 'h5',
-  fontFamily: 'sansSerif',
-  fontSize: 2,
-  fontWeight: 3,
-  textAlign: 'left',
-  my: 3,
-};
-Heading.h6 = styled(Heading)``;
-Heading.h6.defaultProps = {
-  theme: defaultTheme,
-  as: 'h6',
-  fontFamily: 'sansSerif',
-  fontSize: 1,
-  fontWeight: 3,
-  textAlign: 'left',
-  my: 3,
 };
 
 Heading.displayName = 'Heading';
+
+// H1
+Heading.h1 = styled(Heading)``;
+Heading.h1.defaultProps = {
+  ...defaultProps,
+  as: 'h1',
+  fontSize: 6,
+};
+Heading.h1.displayName = 'h1';
+
+// H2
+Heading.h2 = styled(Heading)``;
+Heading.h2.defaultProps = {
+  ...defaultProps,
+  as: 'h2',
+  fontSize: 5,
+};
+Heading.h2.displayName = 'h2';
+
+// H3
+Heading.h3 = styled(Heading)``;
+Heading.h3.defaultProps = {
+  ...defaultProps,
+  as: 'h3',
+  fontSize: 4,
+};
+Heading.h3.displayName = 'h3';
+
+// H4
+Heading.h4 = styled(Heading)``;
+Heading.h4.defaultProps = {
+  ...defaultProps,
+  as: 'h4',
+  fontSize: 3,
+};
+Heading.h4.displayName = 'h4';
+
+// H5
+Heading.h5 = styled(Heading)``;
+Heading.h5.defaultProps = {
+  ...defaultProps,
+  as: 'h5',
+  fontSize: 2,
+};
+Heading.h5.displayName = 'h5';
+
+// H6
+Heading.h6 = styled(Heading)``;
+Heading.h6.defaultProps = {
+  ...defaultProps,
+  as: 'h6',
+  fontSize: 1,
+};
+Heading.h6.displayName = 'h6';
 
 export default Heading;
