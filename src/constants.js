2a6998fcddd7b20c40603bf0ad5f9dab178d0f78
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-05-24 21:27:51 +0200

    Image width fixes (#284)
    * refactor Heading components
    
    * move styled system prop sets into their own file
    
    * add propTypes to Heading
    
    * refactor Text component to use new constants objects
    
    * add layout object to commons file
    
    * updated snapshots
    
    * updated heading names
    
    * update example to render heading component
    
    * added objects for position, borders, and flexbox props
    
    * updated Box to use new system props objects
    
    * updated snapshot tests
    
    * refactor Flex component
    
    - removes flex properties from box.
    - adds Flex.Item subcomponent
    - adds Flex.Row subcomponent
    - adds Flex.Column subcomponent
    
    * updated snapshot tests
    
    * fix image default props
    
    * update CodeBloc component
    
    * update Image story
    
    * update snapshots
    
    * Revert "updated snapshot tests"
    
    This reverts commit 3fed4ba797d2351277e689c8d3bf0da31d08b833.
    
    * Revert "refactor Flex component"
    
    This reverts commit 0fb87b3d6e6746b4bafeb599dca68789e66853b3.
    
    * update snapshots

--- src/constants.js
+++ src/constants.js
@@ -8,6 +8,42 @@ import {
   lineHeight,
   textAlign,
   letterSpacing,
+  // layout
+  display,
+  size,
+  width,
+  height,
+  minWidth,
+  minHeight,
+  maxWidth,
+  maxHeight,
+  overflow,
+  verticalAlign,
+  // position
+  position,
+  zIndex,
+  top,
+  right,
+  bottom,
+  left,
+  // borders
+  borders,
+  borderColor,
+  borderRadius,
+  boxShadow,
+  // flexbox
+  flexBasis,
+  flexDirection,
+  flexWrap,
+  alignContent,
+  alignItems,
+  justifyContent,
+  justifyItems,
+  // flexitem
+  flex,
+  justifySelf,
+  alignSelf,
+  order,
 } from 'styled-system';
 
 export const COMMON = compose(
@@ -22,4 +58,48 @@ export const TYPOGRAPHY = compose(
   lineHeight,
   textAlign,
   letterSpacing
+);
+export const LAYOUT = compose(
+  display,
+  size,
+  width,
+  height,
+  minWidth,
+  minHeight,
+  maxWidth,
+  maxHeight,
+  overflow,
+  verticalAlign
+);
+
+export const POSITION = compose(
+  position,
+  zIndex,
+  top,
+  right,
+  bottom,
+  left
+);
+
+export const BORDERS = compose(
+  borders,
+  borderColor,
+  borderRadius,
+  boxShadow
+);
+
+export const FLEXBOX = compose(
+  // container
+  flexBasis,
+  flexDirection,
+  flexWrap,
+  alignContent,
+  alignItems,
+  justifyContent,
+  justifyItems,
+  // item
+  flex,
+  justifySelf,
+  alignSelf,
+  order
 );
