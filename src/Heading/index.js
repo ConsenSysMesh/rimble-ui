530bcc24e863c14b80457e4bb3d5420eba43269e
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-05-24 14:37:17 +0200

    updated heading names

--- src/Heading/index.js
+++ src/Heading/index.js
@@ -39,7 +39,7 @@ Heading.h1.defaultProps = {
   as: 'h1',
   fontSize: 6,
 };
-Heading.h1.displayName = 'h1';
+Heading.h1.displayName = 'Heading.h1';
 
 // H2
 Heading.h2 = styled(Heading)``;
@@ -48,7 +48,7 @@ Heading.h2.defaultProps = {
   as: 'h2',
   fontSize: 5,
 };
-Heading.h2.displayName = 'h2';
+Heading.h2.displayName = 'Heading.h2';
 
 // H3
 Heading.h3 = styled(Heading)``;
@@ -57,7 +57,7 @@ Heading.h3.defaultProps = {
   as: 'h3',
   fontSize: 4,
 };
-Heading.h3.displayName = 'h3';
+Heading.h3.displayName = 'Heading.h3';
 
 // H4
 Heading.h4 = styled(Heading)``;
@@ -66,7 +66,7 @@ Heading.h4.defaultProps = {
   as: 'h4',
   fontSize: 3,
 };
-Heading.h4.displayName = 'h4';
+Heading.h4.displayName = 'Heading.h4';
 
 // H5
 Heading.h5 = styled(Heading)``;
@@ -75,7 +75,7 @@ Heading.h5.defaultProps = {
   as: 'h5',
   fontSize: 2,
 };
-Heading.h5.displayName = 'h5';
+Heading.h5.displayName = 'Heading.h5';
 
 // H6
 Heading.h6 = styled(Heading)``;
@@ -84,6 +84,6 @@ Heading.h6.defaultProps = {
   as: 'h6',
   fontSize: 1,
 };
-Heading.h6.displayName = 'h6';
+Heading.h6.displayName = 'Heading.h6';
 
 export default Heading;
