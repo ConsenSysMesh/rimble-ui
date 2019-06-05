40847fe79162cbc73c3a56400f54edf3226ee3a4
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-06-05 14:07:50 +0200

    added firefox styles

--- src/Slider/index.js
+++ src/Slider/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { themeGet } from 'styled-system';
 
-import defaultTheme from '../theme';
+import theme from '../theme';
 
 const Slider = styled.input.attrs(props => ({
   type: 'range',
@@ -12,6 +12,7 @@ const Slider = styled.input.attrs(props => ({
     appearance: none;
     height: 2rem;
     min-width: 200px;
+    width: auto;
   }
   &::-webkit-slider-runnable-track {
     box-sizing: border-box;
@@ -32,7 +33,34 @@ const Slider = styled.input.attrs(props => ({
     width: 1.5rem;
     border-radius: 100%;
     cursor: grab;
+    &:hover {
+      box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    }
+    &:active {
+      cursor: grabbing;
+      border-color: ${props => props.primarycolor};
+    }
+  }
 
+  &::-moz-range-track {
+    box-sizing: border-box;
+    appearance: none;
+    border: none;
+    border-radius: 4px;
+    background: ${props => props.primarycolor};
+    height: 4px;
+    display: flex;
+    align-items: center;
+  }
+  &::-moz-range-thumb {
+    box-sizing: border-box;
+    appearance: none;
+    border: 1px solid #ccc;
+    background: white;
+    height: 1.5rem;
+    width: 1.5rem;
+    border-radius: 100%;
+    cursor: grab;
     &:hover {
       box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
     }
@@ -44,7 +72,7 @@ const Slider = styled.input.attrs(props => ({
 `;
 
 Slider.defaultProps = {
-  theme: defaultTheme,
+  theme,
   m: 0,
   p: 0,
 };
