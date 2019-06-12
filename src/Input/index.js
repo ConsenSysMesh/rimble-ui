ab92d319b91eb23bf5b226c4b8218434160a862d
Jesse <murskyfuller@gmail.com>
2019-06-12 19:19:35 -0400

    Add padding between input text and validation icon

--- src/Input/index.js
+++ src/Input/index.js
@@ -29,12 +29,14 @@ const StyledInput = styled(Box)`
   .was-validated & {
     &:valid {
       border-color: #28C081;
+      padding-right: 3rem;
     }
     &:valid ~ svg.icon-valid {
       visibility: visible;
     }
     &:invalid {
       border-color: #EC9081;
+      padding-right: 3rem;
     }
     &:invalid ~ svg.icon-invalid {
       visibility: visible;
