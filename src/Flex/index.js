478fa372fc791e756bc9d8f0067821cd08b30865
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-06-20 16:02:54 +0100

    refactored Flex component

--- src/Flex/index.js
+++ src/Flex/index.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Box from '../Box';
 
-const Flex = styled(Box)`
-  & {
-    display: flex;
-  }
-`;
+// const Flex = styled(Box)`
+//   & {
+//     display: flex;
+//   }
+// `;
+
+const Flex = props => <Box {...props} />;
 
 Flex.defaultProps = {
   display: 'flex',
