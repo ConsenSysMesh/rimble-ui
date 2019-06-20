0be390f79eb28e98b539cad0cc2e477b8f7b722c
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-06-20 19:35:27 +0100

    refactor Heading components

--- src/Heading/index.js
+++ src/Heading/index.js
@@ -10,13 +10,16 @@ const defaultProps = {
   fontWeight: 3,
   lineHeight: 'title',
   m: 0,
+  p: 0,
 };
 
 // Heading
-const Heading = styled.h1`
-  ${TYPOGRAPHY}
+const StyledHeading = styled.div`
   ${COMMON}
+  ${TYPOGRAPHY}
 `;
+
+const Heading = props => <StyledHeading {...props} />;
 
 Heading.defaultProps = {
   ...defaultProps,
@@ -33,7 +36,7 @@ Heading.propTypes = {
 Heading.displayName = 'Heading';
 
 // H1
-Heading.h1 = styled(Heading)``;
+Heading.h1 = props => <Heading {...props} />;
 Heading.h1.defaultProps = {
   ...defaultProps,
   as: 'h1',
@@ -42,7 +45,7 @@ Heading.h1.defaultProps = {
 Heading.h1.displayName = 'Heading.h1';
 
 // H2
-Heading.h2 = styled(Heading)``;
+Heading.h2 = props => <Heading {...props} />;
 Heading.h2.defaultProps = {
   ...defaultProps,
   as: 'h2',
@@ -51,7 +54,7 @@ Heading.h2.defaultProps = {
 Heading.h2.displayName = 'Heading.h2';
 
 // H3
-Heading.h3 = styled(Heading)``;
+Heading.h3 = props => <Heading {...props} />;
 Heading.h3.defaultProps = {
   ...defaultProps,
   as: 'h3',
@@ -60,7 +63,7 @@ Heading.h3.defaultProps = {
 Heading.h3.displayName = 'Heading.h3';
 
 // H4
-Heading.h4 = styled(Heading)``;
+Heading.h4 = props => <Heading {...props} />;
 Heading.h4.defaultProps = {
   ...defaultProps,
   as: 'h4',
@@ -69,7 +72,7 @@ Heading.h4.defaultProps = {
 Heading.h4.displayName = 'Heading.h4';
 
 // H5
-Heading.h5 = styled(Heading)``;
+Heading.h5 = props => <Heading {...props} />;
 Heading.h5.defaultProps = {
   ...defaultProps,
   as: 'h5',
@@ -78,7 +81,7 @@ Heading.h5.defaultProps = {
 Heading.h5.displayName = 'Heading.h5';
 
 // H6
-Heading.h6 = styled(Heading)``;
+Heading.h6 = props => <Heading {...props} />;
 Heading.h6.defaultProps = {
   ...defaultProps,
   as: 'h6',
