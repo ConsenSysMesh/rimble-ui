f2ccc6242cad46db526d819668ecc26497d5352d
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-06-20 15:40:44 +0100

    refactored Card component

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,36 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { textAlign } from 'styled-system';
 import theme from '../theme';
 import Box from '../Box';
 
-const Card = styled(Box)`
-  & {
-    box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.1);
-    transition: box-shadow 300ms ease;
-  }
-  &:hover {
-    box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.3);
-  }
-
-  ${textAlign}
-`;
+const Card = props => <Box {...props} />;
 
 Card.defaultProps = {
   theme,
-  position: 'relative',
   bg: 'white',
   border: 1,
-  borderColor: '#D6D6D6',
-  mx: 0,
-  my: 2,
-  width: '100%',
+  borderColor: 'light-gray',
+  boxShadow: 2,
+  p: 4,
+  width: 1,
   minWidth: 1,
   height: 'auto',
   minHeight: 1,
-  p: 4,
-  textAlign: 'left',
 };
 
 Card.propTypes = {
