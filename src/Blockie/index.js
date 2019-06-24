8d0c7efe70e75d99ceed3ccea7010dbd3405c915
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-06-24 19:45:16 +0100

    add proptypes and descriptions to Blockie

--- src/Blockie/index.js
+++ src/Blockie/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import blockies from 'ethereum-blockies';
 
 class BlockiesIdenticon extends Component {
@@ -27,6 +28,20 @@ class BlockiesIdenticon extends Component {
     });
   }
 }
+
+BlockiesIdenticon.propTypes = {
+  /**
+   * Sets the options for the blockies identicon
+   */
+  opts: PropTypes.shape({
+    seed: PropTypes.string,
+    color: PropTypes.string,
+    bgcolor: PropTypes.string,
+    size: PropTypes.number,
+    scale: PropTypes.number,
+    spotcolor: PropTypes.string,
+  }),
+};
 
 BlockiesIdenticon.displayName = 'BlockiesIdenticon';
 
