7dae38af1d6037cc908a25d82affdc9f88dd5e0c
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-07-08 17:06:33 -0600

    use emotion for theme provider

--- src/ThemeProvider/index.js
+++ src/ThemeProvider/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
-import styled, { createGlobalStyle } from 'styled-components';
-
-import { ThemeProvider as StyledThemeProvider } from 'emotion-theming';
-
+// import { createGlobalStyle } from 'styled-components';
+import { Global, css } from '@emotion/core';
+import styled from '@emotion/styled';
+import {
+  ThemeProvider as StyledThemeProvider,
+  withTheme,
+} from 'emotion-theming';
 import { normalize, transparentize } from 'polished';
-
 import theme from '../theme';
 
 export const Base = styled.div`
@@ -16,12 +17,31 @@ export const Base = styled.div`
   }
 `;
 
-const GlobalStyle = createGlobalStyle`
+// const StyledGlobal = styled(Global)`
+//   ${normalize()}
+//   html {
+//     box-sizing: border-box;
+//   }
+//   *, *:before, *:after {
+//     box-sizing: inherit;
+//   }
+//   ::selection {
+//     background: ${props => transparentize(0.7, props.theme.colors.primary)};
+//   }
+// `;
+
+// const selectionStyle = props => css`
+//   background: ${props => transparentize(0.7, props.theme.colors.primary)};
+// `;
+
+const globalStyle = css`
   ${normalize()}
   html {
     box-sizing: border-box;
   }
-  *, *:before, *:after {
+  *,
+  *:before,
+  *:after {
     box-sizing: inherit;
   }
   ::selection {
@@ -29,15 +49,20 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const GlobalStyle = () => <Global styles={globalStyle} />;
+// const ThemedGlobalStyle = withTheme(GlobalStyle)
+
 const ThemeProvider = ({ children, theme, ...props }) => {
   return (
     <StyledThemeProvider theme={theme} {...props}>
-      <React.Fragment>
-        <GlobalStyle />
-        <Base children={children} />
-      </React.Fragment>
+      <Base children={children} />
+      <GlobalStyle />
     </StyledThemeProvider>
   );
+};
+
+ThemeProvider.defaultProps = {
+  theme,
 };
 
 export default ThemeProvider;
