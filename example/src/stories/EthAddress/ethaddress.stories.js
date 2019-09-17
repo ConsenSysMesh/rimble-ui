77b38db9a3c566271e2db65e5e0aacfcca5297d3
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-09-17 15:59:37 -0400

    updated stories

--- example/src/stories/EthAddress/ethaddress.stories.js
+++ example/src/stories/EthAddress/ethaddress.stories.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { storiesOf, } from '@storybook/react'
 
-import { EthAddress } from 'rimble-ui';
+import { EthAddress, utils } from 'rimble-ui';
 
-storiesOf('Components/', module)
-  .add('EthAddress', () => (
-    <>
-      <EthAddress address={'0x99cb784f0429efd72wu39fn4256n8wud4e01c7d2'} />
-
-      <EthAddress textLabels address={'0x99cb784f0429efd72wu39fn4256n8wud4e01c7d2'} />
-    </>
+storiesOf('Components/EthAddress', module)
+  .add('icon labels', () => (
+    <EthAddress address={'0x99cb784f0429efd72wu39fn4256n8wud4e01c7d2'} />
+  ))
+  .add('text labels', () => (
+    <EthAddress textLabels address={'0x99cb784f0429efd72wu39fn4256n8wud4e01c7d2'} />
   ))
