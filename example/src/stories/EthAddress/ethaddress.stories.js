2a4c1c141703f0e99f6782f713ee39f9301c5b48
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-09-17 15:46:48 -0400

    updated stories

--- example/src/stories/EthAddress/ethaddress.stories.js
+++ example/src/stories/EthAddress/ethaddress.stories.js
@@ -3,27 +3,11 @@ import { storiesOf, } from '@storybook/react'
 
 import { EthAddress } from 'rimble-ui';
 
-storiesOf('Components/EthAddress', module)
-  .add('EthAddress.ReadOnly', () => (
-    <>
-
-      <EthAddress.ReadOnly
-        my={3}
-        address={'0x99cb784f0429efd72wu39fn4256n8wud4e01c7d2'}
-      />
-
-      <EthAddress.ReadOnly
-        my={3}
-        address={'0x99cb784f0429efd72wu39fn4256n8wud4e01c7d2'}
-        buttonText
-      />
-
-    </>
-  ))
+storiesOf('Components/', module)
   .add('EthAddress', () => (
     <>
       <EthAddress address={'0x99cb784f0429efd72wu39fn4256n8wud4e01c7d2'} />
 
-      <EthAddress truncate address={'0x99cb784f0429efd72wu39fn4256n8wud4e01c7d2'} />
+      <EthAddress buttonText address={'0x99cb784f0429efd72wu39fn4256n8wud4e01c7d2'} />
     </>
   ))
