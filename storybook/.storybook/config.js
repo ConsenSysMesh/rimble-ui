77247d0d201583b5c9e62e21229b5f7831d88248
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-10-02 15:02:38 -0400

    added rimble branding

--- storybook/.storybook/config.js
+++ storybook/.storybook/config.js
@@ -1,4 +1,11 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+import rimbleTheme from './rimbleTheme';
+
+addParameters({
+  options: {
+    theme: rimbleTheme,
+  },
+});
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
