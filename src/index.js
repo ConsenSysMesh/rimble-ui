a774e0cb211332e830b44fb8525c4bae45be16f2
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-10-03 16:00:58 -0400

    re-ordered index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
+export { default as theme } from './theme';
+export { default as ThemeProvider } from './ThemeProvider';
+
+// layout
 export { default as Box } from './Box';
 export { default as Flex } from './Flex';
-export { default as Card } from './Card';
-export { default as Image } from './Image';
-export { default as Button } from './Button';
-export { default as Flash } from './Flash';
-export { default as Form } from './Form';
-export { default as MetaMaskButton } from './MetaMaskButton';
-export { default as UPortButton } from './UPortButton';
+
+// components
 export { default as Avatar } from './Avatar';
 export { default as Blockie } from './Blockie';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
 export { default as Checkbox } from './Checkbox';
+export { default as EthAddress } from './EthAddress';
+export { default as Flash } from './Flash';
+export { default as Form } from './Form';
 export { default as Field } from './Field';
 export { default as Heading } from './Heading';
 export { default as Input } from './Input';
+export { default as Icon } from './Icon';
+export { default as Image } from './Image';
 export { default as Link } from './Link';
+export { default as Loader } from './Loader';
+export { default as Modal } from './Modal';
+export { default as MetaMaskButton } from './MetaMaskButton';
 export { default as Progress } from './Progress';
+export { default as Pill } from './Pill';
+export { default as Portal } from './Portal';
+export { default as QR } from './QR';
 export { default as Radio } from './Radio';
 export { default as Select } from './Select';
 export { default as Slider } from './Slider';
@@ -22,13 +34,5 @@ export { default as Table } from './Table';
 export { default as Text } from './Text';
 export { default as Textarea } from './Textarea';
 export { default as Tooltip } from './Tooltip';
-export { default as theme } from './theme';
-export { default as ThemeProvider } from './ThemeProvider';
-export { default as Icon } from './Icon';
-export { default as Loader } from './Loader';
 export { default as ToastMessage } from './ToastMessage';
-export { default as Pill } from './Pill';
-export { default as Portal } from './Portal';
-export { default as Modal } from './Modal';
-export { default as QR } from './QR';
-export { default as EthAddress } from './EthAddress';
+export { default as UPortButton } from './UPortButton';
