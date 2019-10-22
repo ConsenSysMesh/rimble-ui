a07614fc3b29ea5a06022c3129596f901fccf3ef
Mike Lock <michael.lockwitz@consensys.net>
2019-10-22 23:38:31 +0900

    Update storybook stories with as prop on Headings.

--- storybook/stories/Heading.stories.js
+++ storybook/stories/Heading.stories.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Heading } from 'rimble-ui'
+import { Heading } from 'rimble-ui';
 
 export default {
-  title: 'Heading'
-}
+  title: 'Heading',
+};
 
 export const heading = () => (
-  <Heading>
-    The quick brown fox jumps over the lazy dog
-  </Heading>
-)
+  <>
+    <Heading as={'h1'}>The quick brown fox jumps over the lazy dog</Heading>
+    <Heading as={'h2'}>The quick brown fox jumps over the lazy dog</Heading>
+    <Heading as={'h3'}>The quick brown fox jumps over the lazy dog</Heading>
+    <Heading as={'h4'}>The quick brown fox jumps over the lazy dog</Heading>
+    <Heading as={'h5'}>The quick brown fox jumps over the lazy dog</Heading>
+    <Heading as={'h6'}>The quick brown fox jumps over the lazy dog</Heading>
+    <Heading>The quick brown fox jumps over the lazy dog</Heading>
+  </>
+);
