39895e5eae75328bca530da8cc88186004385a7c
Gustavo Esquinca <g.esq.ca@gmail.com>
2019-11-21 10:58:34 -0500

    organize imports for legibility

--- src/index.js
+++ src/index.js
@@ -2,40 +2,48 @@ export { default as theme } from './theme';
 export { default as ThemeProvider } from './ThemeProvider';
 export { default as BaseStyles } from './BaseStyles';
 
-// layout
+// layout components
 export { default as Box } from './Box';
 export { default as Flex } from './Flex';
 
-// Typography
+// typography components
 export { default as Text } from './Text';
 export { default as Heading } from './Heading';
 
-// components
-export { default as Link } from './Link';
-export { default as Avatar } from './Avatar';
-export { default as Blockie } from './Blockie';
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as Checkbox } from './Checkbox';
-export { default as EthAddress } from './EthAddress';
-export { default as Flash } from './Flash';
+// form components
+export { default as Input } from './Input';
 export { default as Form } from './Form';
 export { default as Field } from './Field';
-export { default as Input } from './Input';
-export { default as Icon } from './Icon';
-export { default as Image } from './Image';
-export { default as Loader } from './Loader';
-export { default as Modal } from './Modal';
-export { default as MetaMaskButton } from './MetaMaskButton';
-export { default as Progress } from './Progress';
-export { default as Pill } from './Pill';
-export { default as Portal } from './Portal';
-export { default as QR } from './QR';
+export { default as Checkbox } from './Checkbox';
 export { default as Radio } from './Radio';
 export { default as Select } from './Select';
 export { default as Slider } from './Slider';
-export { default as Table } from './Table';
 export { default as Textarea } from './Textarea';
+export { default as Progress } from './Progress';
+
+// components
+export { default as Button } from './Button';
+export { default as Link } from './Link';
+export { default as Card } from './Card';
+export { default as Avatar } from './Avatar';
+export { default as Image } from './Image';
+export { default as Icon } from './Icon';
+export { default as Loader } from './Loader';
+export { default as Table } from './Table';
+export { default as Pill } from './Pill';
+export { default as Flash } from './Flash';
+export { default as Modal } from './Modal';
 export { default as Tooltip } from './Tooltip';
 export { default as ToastMessage } from './ToastMessage';
+
+// eth specific
+export { default as EthAddress } from './EthAddress';
+export { default as Blockie } from './Blockie';
+export { default as QR } from './QR';
+
+// branded components
+export { default as MetaMaskButton } from './MetaMaskButton';
 export { default as UPortButton } from './UPortButton';
+
+// utility components
+export { default as Portal } from './Portal';
