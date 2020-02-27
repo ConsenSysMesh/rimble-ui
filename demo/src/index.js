6a15c4bcc9c38645fe9922125f8cd7349d614c95
Gustavo Esquinca <g.esq.ca@gmail.com>
2020-02-27 13:21:32 -0500

    wip, use codefi demo app

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import App from './App';
+// import App from './App';
+import App from './CodefiApp';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
